9e1b9d288bdc26f7c8ff910f914f2a24b0956c34
Ahmad Mujtaba <corporaldavid891@Gmail.com>
2022-12-04 08:06:38 +0500

    Update Learning_Python_Like_Never_before.py

--- Learning_Python_Like_Never_before.py
+++ Learning_Python_Like_Never_before.py
@@ -1,16 +1,16 @@
-#A shorter version of vowel checking program. 
+#A short version of vowel checking program using set. 
 
 #set
 
-vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+vowels = {'a', 'e', 'i',' o', 'u', 'A', 'E', 'I', 'O', 'U'}
 
 word = input("Please type your sentence here: ")
 
-#Execution
-
 found = vowels.intersection(set(word))
 
-print("Here are the vowels in your sentence.")
+#Execution
+
+print("Here are the vowels in the sentence that you provided.")
 
 for vowel in sorted(found):
     print(vowel)
