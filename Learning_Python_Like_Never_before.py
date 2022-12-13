ce87a5d01b41680f81f1bc7dd347a09eda19a846
Ahmad Mujtaba <corporaldavid891@Gmail.com>
2022-12-13 11:35:20 +0500

    Vowel_Check_Function

--- Learning_Python_Like_Never_before.py
+++ Learning_Python_Like_Never_before.py
@@ -1,32 +1,11 @@
-#Dict within dict(Also know as complex data structure.)
+#Creating a function
 
-#importing pprint module. 
+#Vowel check function
 
-import pprint
-
-#dict
-
-people = {'Ford': {'Name': "Ford Prefect",
-                   'Gender': "Male",
-                   'Occupation': "Researcher",
-                   'Home Planet': "Betelgeuse Home"},
-
-          'Arthur': {'Name': "Arthur Dent", 
-                     'Gender': "Male", 
-                     'Occupation': "Sandwich-Maker",
-                     'Home Planet': "Earth"},
-          
-          'Trillian': {'Name': "Tricia McMillan",
-                       'Gender': 'Female',
-                       'Occupation': 'Mathematician',
-                       'Home Planet': 'Earth'},
-
-            'Robot': {'Name': "Marvin",
-                      'Gender': "Unknown",
-                      'Occupation': "Paranoid Android",
-                      'Home Planet': "Unknown"}
-}
-
-#Execution
-
-pprint.pprint(people)
+def Vowel_Check(word):
+    """Check for vowels in a provided requested sentence."""
+    vowels = set('aeiou')
+    found = vowels.intersection(set(word))
+    print('Here are the vowels in your sentence:')
+    for vowel in sorted(found):
+        print(vowel)
